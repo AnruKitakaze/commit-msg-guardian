fdaeca3c0875aa0671ba336d28f534fc9ab2b7ef
Руденов Андрей Петрович <rudenov@tochka.com>
2024-10-27 15:36:44 +0700

    feat: add hook to check for empty line
    Add a hook to make sure that there is
    an empty line before summary line
    and description body.

--- main.go
+++ main.go
@@ -3,16 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	fmt.Println("TEST HOOK")
 	d, err := os.ReadFile("./.git/COMMIT_EDITMSG")
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Print("\n\nABORTION\n\n")
-
 	fmt.Println(string(d))
-	// os.Exit(1)
+
+	ok := true
+	s := strings.Split(string(d), "\n")
+	fmt.Printf("Summary: %v\n", s[0])
+	if len(s) > 1 {
+		if len(s[1]) != 0 {
+			fmt.Println("No empty line between summary and description")
+			ok = false
+		}
+	}
+
+	if !ok {
+		fmt.Println("GOT AN ERROR")
+		os.Exit(1)
+	}
+	fmt.Println("NO ERRORS")
 }
